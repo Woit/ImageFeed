f2efce9f468bf659b9301ec8deaea6c88e60d768
Woit <mitrofanov.a.egor@gmail.com>
2024-05-24 17:02:58 +0400

    init

---
+++ ImageFeed/ImageFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageFeedApp.swift
+//  ImageFeed
+//
+//  Created by Wo It on 24.05.24.
+//
+
+import SwiftUI
+
+@main
+struct ImageFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
